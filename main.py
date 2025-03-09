7cfdabb83632d18480251f8b068251a239ca3c14
Morgan Saunier <morgan.saunier.gg@gmail.com>
2025-03-09 16:51:15 +0100

    use_environ

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 # main.py
+from dotenv import load_dotenv
 import os
+
 from src.data.spt_track_duration import get_spotify_duration
 from src.data.yt_vid_duration import get_video_details_from_youtube
 from src.utils.estimate_story_length import spotify_story_length
-from src.config.env_loader import env_loader
+
+load_dotenv()  # Load env variables once
 
 def get_duration(service, track_url):
     if service == "youtube":
-        return get_video_details_from_youtube(track_url, os.getenv("YOUTUBE_API_KEY"))
+        return get_video_details_from_youtube(track_url, os.environ["YOUTUBE_API_KEY"])
     elif service == "spotify":
         return get_spotify_duration(
             track_url,
-            os.getenv("SPOTIFY_CLIENT_ID"),
-            os.getenv("SPOTIFY_CLIENT_SECRET")
+            os.environ["SPOTIFY_CLIENT_ID"],
+            os.environ["SPOTIFY_CLIENT_SECRET"]
         )
     return None
 
 def main():
+    env_loader()
     service = input("Choose service (YouTube/Spotify): ").strip().lower()
     track_url = input("Enter the track URL: ").strip()
 
--- src/config/env_loader.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-def env_loader():
-    load_dotenv()
-    return {
-        "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-        "SPOTIFY_CLIENT_ID": os.getenv("SPOTIFY_CLIENT_ID")
-    }
